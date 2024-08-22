09308485288e83d03eaac28d4b543c3fa2f8d10d
sebins <bysws302@naver.com>
2024-08-22 21:30:57 +0900

    싱글톤 패턴

--- src/test/java/hello/singleton/SingletonTest.java
+++ src/test/java/hello/singleton/SingletonTest.java
@@ -26,4 +26,16 @@ public class SingletonTest {
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
 
     }
+
+    @Test
+    @DisplayName("싱그론 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
